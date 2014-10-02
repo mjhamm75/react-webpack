e38f14867b5f3ac957944edfaddbf4a51af2ee6a
Jason Hamm <jasonhamm.me@gmail.com>
2014-10-02 06:08:32 -0600

    Add react-router to project

---
+++ app.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var Router = require('react-router');
+var Routes = Router.Routes;
+var Route = Router.Route;
+      
+React.renderComponent(
+	<Routes>
+		<Route>
+
+		</Route>
+	</Routes>,
+	document.getElementById('example')
+);
